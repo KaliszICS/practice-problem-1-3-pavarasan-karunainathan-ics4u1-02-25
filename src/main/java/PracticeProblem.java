e8804aa62317137911c9e276534cf5a628e08c95
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-03-05 15:01:34 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,46 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+	static String readFile(String filename){
+		FileReader inputStream = null;
+		try{
+			inputStream = new FileReader(filename);
+			String str = "";
+			char c = '\0';
+			while((c = (char)inputStream.read()) != (char)-1){
+				str += c;
+			}
+			str = str.stripTrailing(); // ????
+			inputStream.close();
+			return str;
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		return "";
 	}
 
-	
+	static String backwardsReadFile(String filename){
+		FileReader inputStream = null;
+		try{
+			inputStream = new FileReader(filename);
+			String str = "";
+			char c = '\0';
+			while((c = (char)inputStream.read()) != (char)-1){
+				str += c;
+			}
+			inputStream.close();
+			str = str.stripTrailing(); // ????
+			String reversed = "";
+			for(int i = str.length()-1; i > -1; --i){
+				reversed += str.charAt(i);
+			}
+			return reversed;
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		return "";
+	}
 
+	public static void main(String args[]){}
 }
